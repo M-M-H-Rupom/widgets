534dc4f8cc1781b15bfc1f6048d035332ebff9b6
rupom <mohrajulrupom@gmail.com>
2024-04-05 00:18:34 +0600

    dashboard widgets

--- demo_widget.php
+++ demo_widget.php
@@ -11,18 +11,25 @@ class demo_widget extends WP_Widget{
         $title = isset($instance['title']) ? $instance['title'] : 'Demo widget';
         $latitude = isset($instance['latitude']) ? $instance['latitude'] : 23.9;
         $longitude = isset($instance['longitude']) ? $instance['longitude'] : 90.8;
+        $email = isset($instance['email']) ? $instance['email'] : '';
         ?>
-        <p>
-            <label for="<?php echo $this->get_field_id('title') ?>;"> Title </label>
-            <input type="text" class="widefat" id="<?php echo $this->get_field_id('title') ?>;" name="<?php echo $this->get_field_name('title') ?>" value="<?php echo $title ?>">
-        </p>
-        <p>
-            <label for="<?php echo $this->get_field_id('latitude') ?>;"> latitude </label>
-            <input type="text" class="widefat" id="<?php echo $this->get_field_id('latitude') ?>;" name="<?php echo $this->get_field_name('latitude') ?>" value="<?php echo $latitude ?>">
-        </p>
+        <div class="demo_widget field_column">
+            <p class="">
+                <label for="<?php echo $this->get_field_id('title') ?>; "> Title </label>
+                <input type="text" class="widefat" id="<?php echo $this->get_field_id('title') ?>;" name="<?php echo $this->get_field_name('title') ?>" value="<?php echo $title ?>">
+            </p>
+            <p>
+                <label for="<?php echo $this->get_field_id('latitude') ?>;"> latitude </label>
+                <input type="text" class="widefat" id="<?php echo $this->get_field_id('latitude') ?>;" name="<?php echo $this->get_field_name('latitude') ?>" value="<?php echo $latitude ?>">
+            </p>
+        </div>
         <p>
             <label for="<?php echo $this->get_field_id('longitude') ?>;"> longitude </label>
             <input type="text" class="widefat" id="<?php echo $this->get_field_id('longitude') ?>;" name="<?php echo $this->get_field_name('longitude') ?>" value="<?php echo $longitude ?>">
+        </p>
+        <p>
+            <label for="<?php echo $this->get_field_id('email') ?>;"> email </label>
+            <input type="text" class="widefat" id="<?php echo $this->get_field_id('email') ?>;" name="<?php echo $this->get_field_name('email') ?>" value="<?php echo $email ?>">
         </p>
         <?php
     }
@@ -36,14 +43,26 @@ class demo_widget extends WP_Widget{
             <div class="demowidget">
                 <p> Latitude : <?php echo isset($instance['latitude']) ? $instance['latitude'] : '' ?></p>
                 <p> longitude : <?php echo isset($instance['longitude']) ? $instance['longitude'] : '' ?></p>
+                <p> email : <?php echo isset($instance['email']) ? $instance['email'] : '' ?></p>
             </div>
             <?php
         }
         echo $args['after_widget'];
         
     }
-    // public function update(){
-        
-    // }
+    public function update($new_instance,$old_instance){
+        $instance = $new_instance;
+        $instance['title'] = sanitize_text_field($instance['title']);
+        if(!is_email($new_instance['email'])){
+            $instance['email'] = $old_instance['email'];
+        }
+        if(!is_numeric($new_instance['latitude'])){
+            $instance['latitude'] = $old_instance['latitude'];
+        }
+        if(!is_numeric($new_instance['longitude'])){
+            $instance['longitude'] = $old_instance['longitude'];
+        }
+        return $instance;
+    }
 }
 ?>
--- index.php
+++ index.php
@@ -11,4 +11,54 @@ function widget_register(){
     register_widget('demo_widget');
 }
 add_action('widgets_init','widget_register');
+
+function callback_widget_style($screen){
+    if($screen == 'widgets.php'){
+        wp_enqueue_style('widget_css', plugin_dir_url( __FILE__ ).'/css/style.css' );
+    }
+}
+add_action( 'admin_enqueue_scripts','callback_widget_style');
+
+// dashboard widget
+function dw_dashboard_widget1(){
+    wp_add_dashboard_widget('dash_widget1', 'dashboard_widget1', 'callback_for_output1');
+}
+add_action('wp_dashboard_setup','dw_dashboard_widget1');
+
+function callback_for_output1(){
+   echo 'hello dashboard';
+}
+// second widget
+function dw_dashboard_widget2(){
+    if(current_user_can('edit_dashboard')){
+        wp_add_dashboard_widget('dash_widget2', 'dashboard_widget2', 'callback_for_output2','dashboard_configure');
+    }
+    wp_add_dashboard_widget('dash_widget2', 'dashboard_widget2', 'callback_for_output2');
+}
+add_action('wp_dashboard_setup','dw_dashboard_widget2');
+
+function callback_for_output2(){
+   $feeds = array(
+        array(
+            'url' => 'http://localhost/plugin_development/feed/',
+            'items' => 3,
+            'show_author' => 0,
+            'show_date' => 0,
+        ) 
+   );
+   wp_dashboard_primary_output('dash_board_wid', $feeds);
+}
+function dashboard_configure(){
+    $option_value = get_option('dash_board_wid_nop',3);
+    if(isset($_POST['dashboard-widget-nonce'])){
+        if(isset($_POST['dw_nop'])){
+            $option_value = sanitize_text_field($_POST['dw_nop']);
+            update_option('dash_board_wid_nop',$option_value);
+        }
+    }
+    ?>
+    <label for="">Number of post:</label> <br>
+    <input type="text" name="dw_nop" id="dw_nop" value="<?php echo $option_value ?>"> <br>
+    <?php
+}
 ?>
